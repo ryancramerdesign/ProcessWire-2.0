20718ea7f7e287b99c4a009d2ce3cc883371801b
Ryan Cramer <ryan@rc-d.net>
2010-11-04 09:26:11 -0400

    Updates for sending to GitHub.

--- README.txt
+++ README.txt
@@ -1,8 +1,25 @@
-ProcessWire 2.0 Installation
+ABOUT PROCESSWIRE
+---------------------------------------------------------------------------
+ProcessWire is an open source content management system (CMS) and web 
+application framework aimed at the needs of designers, developers and their 
+clients. ProcessWire gives you more control over your fields, templates and 
+markup than other platforms, and provides a powerful template system that 
+works the way you do. Not to mention, ProcessWire's API makes working with 
+your content easy and enjoyable. Managing and developing a site in 
+ProcessWire is shockingly simple compared to what you may be used to.
+
+
+ABOUT THIS VERSION
+---------------------------------------------------------------------------
+This version is a developer preview, primarily for testing and introducing
+ProcessWire to other web designers and developers. This version is not 
+intended for production use and there are likely to be some changes before 
+the stable release. Please contact Ryan with any bugs or issues you may
+encounter at http://ryancramer.com/contact/
+
 
 REQUIREMENTS
-------------
-
+---------------------------------------------------------------------------
 1. A unix-based web server running Apache. 
 2. PHP version 5.2.3 or greater.
 3. MySQL 5.0.15 or greater.
@@ -10,18 +27,38 @@ REQUIREMENTS
 5. Apache must support .htaccess files. 
 
 
-INSTALLATION
-------------
+INSTALLATION FROM ZIP
+---------------------------------------------------------------------------
 
 1. Unzip the ProcessWire installation file to the location where you want it
-   installed on your web server. Ensure that the directory structure and file 
-   permissions are maintained. 
+   installed on your web server. 
 
 2. Load the location that you unzipped (or uploaded) the files to in your web
    browser. This will initiate the ProcessWire installer. The installer will
    guide you through the rest of the installation.
 
-If you run into any errors or questions, contact Ryan at:
+
+INSTALLATION FROM GIT
+---------------------------------------------------------------------------
+
+1. Git clone ProcessWire to the place where you want to install it. 
+
+2. Because ProcessWire will be available with different site profiles, 
+   a site profile is not included with the source on Git. You can download
+   the site profile from http://processwire.com/download/ 
+
+   Unzip the site profile to the same directory where you installed 
+   ProcessWire, and it should create a directory called '/site/'
+
+3. Load the location where you installed ProcessWire into your browser. 
+   This will initiate the ProcessWire installer. The installer will guide
+   you through the rest of the installation.  
+
+
+HAVE QUESTIONS, NEED HELP, OR FOUND A BUG?
+------------------------------------------
+Please contact Ryan at: 
 http://www.ryancramer.com/contact/ 
 
 
+ProcessWire, Copyright 2010 by Ryan Cramer
--- index.php
+++ index.php
@@ -21,7 +21,17 @@
 
 define("PROCESSWIRE", 200); 
 
+/*
+ * If you include ProcessWire's index.php from another script, or from a
+ * command-line script, the $wire variable is your connection to the API.
+ *
+ */
 $wire = null;
+
+/*
+ * Define installation paths and urls
+ *
+ */
 $rootPath = dirname(__FILE__);
 $rootURL = isset($_SERVER['HTTP_HOST']) ? substr($rootPath, strlen($_SERVER['DOCUMENT_ROOT'])) . '/' : '/';
 $wireDir = 'wire';
@@ -29,10 +39,16 @@ $coreDir = "$wireDir/core";
 $siteDir = "site";
 $assetsDir = "$siteDir/assets";
 
-// setup ProcessWire class autoloads
+/*
+ * Setup ProcessWire class autoloads
+ *
+ */
 require("$rootPath/$coreDir/ProcessWire.php");
 
-// setup configuration data and default paths/urls
+/*
+ * Setup configuration data and default paths/urls
+ *
+ */
 $config = new Config();
 $config->urls = new Paths($rootURL); 
 $config->urls->modules = "$wireDir/modules/";
@@ -48,15 +64,24 @@ $config->paths = clone $config->urls;
 $config->paths->root = $rootPath . '/';
 $config->paths->sessions = $config->paths->assets . "sessions/";
 
-// styles and scripts are CSS and JS files, as used by the admin application
-// may be used by other apps and templates as needed too
+/*
+ * Styles and scripts are CSS and JS files, as used by the admin application.
+ * But reserved here if needed by other apps and templates.
+ *
+ */
 $config->styles = new FilenameArray();
 $config->scripts = new FilenameArray();
 
-// include user-specified configuration options
+/*
+ * Include user-specified configuration options
+ *
+ */
 require($rootPath . '/' . $siteDir . "/config.php"); 
 
-// if debug mode is on then echo all errors, if not then disable all error reporting
+/*
+ * If debug mode is on then echo all errors, if not then disable all error reporting
+ *
+ */
 if($config->debug) {
 	error_reporting(E_ALL | E_STRICT); 
 	ini_set('display_errors', 1);
@@ -65,51 +90,83 @@ if($config->debug) {
 	ini_set('display_errors', 0);
 }
 
-// If PW2 installed installed, go to the installer
+/*
+ * If PW2 is not installed, go to the installer
+ *
+ */
 if(!$config->dbName && is_file("./install.php") && $_SERVER['REQUEST_URI'] == $rootURL) {
 	require("./install.php");
 	exit(0);
 }
 
-// prepare session variables, kept here out in the open in case you need to change them
+/*
+ * Prepare any PHP ini_set options
+ *
+ */
 session_name($config->sessionName); 
 ini_set('session.use_cookies', true); 
 ini_set('session.use_only_cookies', 1);
 ini_set("session.gc_maxlifetime", $config->sessionExpireSeconds); 
 ini_set("session.save_path", $config->paths->sessions); 
 
-// load and execute ProcessWire
+/*
+ * Load and execute ProcessWire
+ *
+ */
 try { 
-	// bootstrap ProcessWire's core and make the API available
+	/*
+	 * Bootstrap ProcessWire's core and make the API available with $wire
+	 *
+	 */
 	$wire = new ProcessWire($config); 
 
-	// store the admin URL in the configuration
+	/*
+	 * Store the admin URL in the configuration
+	 *
+	 */
 	if($config->adminRootPageID) $config->urls->admin = $wire->pages->get($config->adminRootPageID)->url; 
 
-	// if we're not being called from another shell script or PHP page, then run the PageView process
+	/* 
+	 * If we're not being called from another shell script or PHP page, then run the PageView process
+	 *
+	 */
 	if(isset($_SERVER['HTTP_HOST']) && realpath(__FILE__) == realpath($_SERVER['SCRIPT_FILENAME'])) {
 		$controller = new ProcessController(); 
 		$controller->setProcessName("ProcessPageView"); 
 		echo $controller->execute();
+
 	} else {
-		// some other script included this for non-http use or access to the API without rendering any pages at this time. 
-		// The global $wire var may be used to access the API after including this file. 
+		/*
+		 * Some other script included this for non-http use or access to the API without 
+		 * rendering any pages at this time. The $wire var may be used to access 
+		 * the API after including this file. 
+		 *
+		 */
 	}
 
 } catch(Exception $e) {
 
-	// display errors if debug mode is on or if superuser logged in 
+	/*
+	 * Display errors if debug mode is on or if superuser logged in 
+	 *
+	 */
 	if($config->debug || ($wire && $wire->user && $wire->user->isSuperuser())) {
 		if(isset($_SERVER['HTTP_HOST'])) echo "<pre>";
 		echo "ProcessWire Exception: " . $e->getMessage() . "\n\n" . $e->getTraceAsString();
 		echo "\n\nNote: This error message was shown because debug mode is on or you are logged in as a superuser.\n\n";
 	} else {
-		// public facing users just get a http 500 error
+		/* 
+		 * Public facing users just get a http 500 error
+		 *
+		 */
 		header("HTTP/1.1 500 Internal Server Error"); 
 		echo "Internal Server Error";
 	}
 
-	// log the exception
+	/*
+	 * Log the exception
+	 *
+	 */
 	$log = new FileLog($config->paths->logs . "errors.txt"); 
 	$log->save(($wire && $wire->user ? $wire->user->name : 'unknown') . ': ' . ($wire && $wire->page ? $wire->page->path : '/?/') . ': ' . $e->getMessage()); 
 
