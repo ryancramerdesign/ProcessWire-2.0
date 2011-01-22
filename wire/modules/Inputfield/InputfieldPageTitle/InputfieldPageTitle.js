4bc0e1136b067bd663426a544a207aab6704147d
Ryan Cramer <ryan@rc-d.net>
2011-01-21 22:45:46 -0500

    Fix to JS for name field when editing homepage. Also now supports more extended characters for converting to URL name.

--- wire/modules/Inputfield/InputfieldPageTitle/InputfieldPageTitle.js
+++ wire/modules/Inputfield/InputfieldPageTitle/InputfieldPageTitle.js
@@ -1,7 +1,50 @@
+﻿/**
+ * Convert a title/headline to an ASCII URL name
+ * 
+ * 1. Convert accented characters to the ASCII equivalent. 
+ * 2. Convert non -_a-z0-9. to blank. 
+ * 3. Replace multiple dashes with single dash. 
+ *
+ */
+function titleToUrlName(name) {
+
+	// replace leading and trailing whitespace 
+	name = jQuery.trim(name).toLowerCase();  
+  
+	// utf8 accented to ascii translation
+	var str1 = ":,àáäâèéëêìíïîòóöôùúüûñçčćďĺľńňŕřšťž";
+	var str2 = "--aaaaeeeeiiiioooouuuuncccdllnnrrstz";
+
+	// change common accented characters to ascii equivalent
+	for(var cnt=0, n=str1.length; cnt<n; cnt++) {
+		var re = new RegExp(str1.charAt(cnt), 'g'); 
+		name = name.replace(re, str2.charAt(cnt));
+	}
+
+	// replace invalid with blank
+	name = name.replace(/[^-a-z0-9. ]/g, '');
+
+	// convert whitespace to dash
+	name = name.replace(/\s+/g, '-') 
+
+	// convert multiple dashes to single
+	name = name.replace(/--+/g, '-'); 
+
+	// remove leading or trailing dashes
+	name = name.replace(/(^-|-$)/g, ''); 
+
+	return name;
+}
+
 $(document).ready(function() {
 
 	var $nameField = $("#Inputfield_name"); 
-	if($nameField.val().length) return; // no need to continue since it already has a value
+
+	// check if namefield exists, because pages like homepage don't have one
+	if($nameField.size() < 1) return;
+	
+	// no need to continue if it already has a value	
+	if($nameField.val().length) return; 
 
 	var $titleField = $(".InputfieldPageTitle input[type=text]"); 
 	var active = true; 
@@ -9,8 +52,8 @@ $(document).ready(function() {
 	var titleKeyup = function() {
 		if(!active) return; 
 		var val = $titleField.val().substring(0, 70); 
-		var re = new RegExp(/([^_a-z0-9]+)/ig); 
-		$nameField.val(val.replace(re, '-')).trigger('keyup'); 
+		$nameField.val(titleToUrlName(val)).trigger('keyup'); 
+		
 	}
 
 	$titleField.keyup(titleKeyup); 
@@ -19,5 +62,5 @@ $(document).ready(function() {
 		// if they happen to change the name field on their own, then disable 
 		if($(this).val().length) active = false;
 	}); 
-	
+		
 }); 
