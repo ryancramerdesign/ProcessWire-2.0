8b58da2ae124b517f0c83d681af8d36e8b3cccbe
Ryan Cramer <ryan@rc-d.net>
2012-02-14 09:44:18 -0500

    This is just a test. Nothing to see here, but thanks for watching.

--- site-default/templates/page.php
+++ site-default/templates/page.php
@@ -7,9 +7,7 @@
 
 include("./head.inc"); 
 
-// Not really necessary, but wanted the markup to line up with where it left off in header include,
-// so put four tabs before the bodycopy before outputting it: 
-echo "\t\t\t\t{$page->body}";
+echo $page->body;
 
 include("./foot.inc"); 
 
